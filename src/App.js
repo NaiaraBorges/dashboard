6762f6dcdf9ef2faa275d771329e8e25ce7a8138
Naiara Borges <naiarazk@gmail.com>
2022-12-14 17:45:58 -0300

    Criando Context Api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Navbar, Sidebar } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor} from './pages';
+
+import { useStateContext } from './contexts/ContextProvider';
 
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
